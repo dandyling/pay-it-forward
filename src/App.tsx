3d72d9a8414ac96e47705b25161fb74bebf1bfdc
dandyling <dandyling@gmail.com>
2024-11-10 14:02:19 +0800

    Add shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <main>
       <h1 className="text-4xl font-bold">Pay it forward</h1>
+      <Button>Click me</Button>
     </main>
   );
 }
