8b160910b905c003d251dffee40578438c1c9574
dandyling <dandyling@gmail.com>
2024-11-11 00:56:01 +0800

    Add adjust height

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -17,6 +17,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     };
 
     useEffect(() => {
+      adjustHeight();
       // This will make it run after the event loop
       const timer = setTimeout(adjustHeight, 0);
       return () => clearTimeout(timer);
